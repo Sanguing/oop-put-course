7723677fd06c32e294bbcb6e9ee93936162d70bc
Sanguing <115477382+Sanguing@users.noreply.github.com>
2022-10-24 16:50:27 +0200

    Create ex-2.cpp

---
+++ lesson-2/ex-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+class milk {
+    std::string name;
+    float fat_percentage;
+    std::string color;
+
+public:
+
+    milk()  {      
+        std::string name = "Milk";
+        fat_percentage = 0;
+        std::string color = "White";
+        std::cout << "Default";
+    }
+
+    milk(std::string name) {
+        this->name = name;
+        std::cout << "Poorly defined milk";
+    }
+
+    milk(std::string name, float fat_percentage) {
+        this->name = name;
+        this->fat_percentage = fat_percentage;
+        std::cout << "Average defined milk";
+    }
+    milk(std::string name, float fat_percentage, std::string color) {
+        this->name = name;
+        this->fat_percentage = fat_percentage;
+        this->color = color;
+        std::cout << "Well defined milk";
+    }
+
+    void display()
+    {
+        std::cout << "\n" <<name << "\n" << fat_percentage << "\n" << color << "\n";
+    }
+
+    };
+
+    int main()
+    {
+        milk* tasty_milk = new milk("milk_no_1", 5, "black");
+        tasty_milk->display();
+        
+    }
