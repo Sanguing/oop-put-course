ba2fbb99ed15d106ce7779537d3d5e11a3609ad1
Sanguing <115477382+Sanguing@users.noreply.github.com>
2022-12-12 23:55:54 +0100

    Delete exercise - 8 directory

--- exercise - 8/src/Forecast.java
+++
@@ -1,9 +0,0 @@
-public interface Forecast {
-    float f_temperature();
-    final class Fake implements Forecast {
-        @Override
-        public float f_temperature(){
-            return 100.0f;
-        }
-    }
-}
